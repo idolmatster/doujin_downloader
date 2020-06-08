0ba4f5940af3e8a3d6ae4b608dd39edf40f1d636
Zhāng Jūn <60116198+idolmatster@users.noreply.github.com>
2020-06-09 01:49:01 +0200

    updated to accomodate png or mixed format galaries

--- xtest.sh
+++ xtest.sh
@@ -12,17 +12,21 @@ cd $3
 for I in `seq 1 $2`
 do
     url="https://i.nhentai.net/galleries/$1/$I.jpg"
-    curl ${url} -O
+    alturl="https://i.nhentai.net/galleries/$1/$I.png"
+    wget ${url} || wget ${alturl}
 done
 
-#create file to remark nid
-echo "http://nhentai.net/g/$3/" > "$3.no"
+
 
 #oneliner lmao https://stackoverflow.com/a/3672383
 ls | awk '/^([0-9]+)\.jpg$/ { printf("%s %05d.jpg\n", $0, $1) }' | xargs -n2 mv
+ls | awk '/^([0-9]+)\.png$/ { printf("%s %05d.png\n", $0, $1) }' | xargs -n2 mv
 
 #PDFer
-podofoimg2pdf "$3.pdf" -useimgsize *.jpg
+podofoimg2pdf "$3.pdf" -useimgsize *.*
+
+#create file to remark nid
+echo "http://nhentai.net/g/$3/" > "$3.no"
 
 #zip it
 zip "$3.zip" *
