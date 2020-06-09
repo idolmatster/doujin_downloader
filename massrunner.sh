bbaf7b1eb6fac064a8e6d751d7a4809f8b33bdce
Zhāng Jūn <60116198+idolmatster@users.noreply.github.com>
2020-06-09 12:13:40 +0200

    Delete massrunner.sh

--- massrunner.sh
+++
@@ -1,7 +0,0 @@
-scriptdllist="./continuefile"
-    while IFS= read -r line
-    do
-        ./xtest.sh ${line}
-        echo "${line}" >> done.log
-        sed -i '1d' continuefile
-    done <"$scriptdllist"
