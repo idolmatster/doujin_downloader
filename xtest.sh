7c6dccbcfcf873d1609180590009e7c8352d8817
Zhāng Jūn <60116198+idolmatster@users.noreply.github.com>
2020-08-18 22:18:54 +0200

    Update xtest.sh

--- xtest.sh
+++ xtest.sh
@@ -19,8 +19,6 @@ do
     wget ${url} || wget ${alturl}
 done
 
-
-
 #oneliner lmao https://stackoverflow.com/a/3672383
 ls | awk '/^([0-9]+)\.jpg$/ { printf("%s %05d.jpg\n", $0, $1) }' | xargs -n2 mv
 ls | awk '/^([0-9]+)\.png$/ { printf("%s %05d.png\n", $0, $1) }' | xargs -n2 mv
@@ -37,6 +35,7 @@ podofoimg2pdf "$3.pdf" -useimgsize *.* || convert
 
 #create file to remark nid
 echo "http://nhentai.net/g/$3/" > "$3.no"
+curl -s "https://nhentai.net/api/gallery/$3" | jq '.title.pretty' >> "$3.no" || echo "please install jq"
 
 #zip it
 zip "$3.zip" *
